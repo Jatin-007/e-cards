982b89849e01fc5e67857ef75b2943c2f7f66820
JATIN <s.jatin1903@gmail.com>
2019-02-19 09:41:35 -0800

    dynamic file types

---
+++ src/MainScreen.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import randomcolor from 'randomcolor';
+import {StyleSheet,Text,View} from 'react-native';
+  
+import Swiper from 'react-native-swiper';
+import SwiperLeft from './SwiperLeft';
+import SwiperRight from './SwiperRight';
+  
+//   const styles = StyleSheet.create({
+//     wrapper: {
+//     },
+//     slide1: {
+//       flex: 1,
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//       backgroundColor: '#9DD6EB',
+//     },
+//     slide2: {
+//       flex: 1,
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//       backgroundColor: '#97CAE5',
+//     },
+//     slide3: {
+//       flex: 1,
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//       backgroundColor: '#92BBD9',
+//     },
+//     text: {
+//       color: '#fff',
+//       fontSize: 30,
+//       fontWeight: 'bold',
+//     }
+//   })
+  
+class MainScreen extends Component {
+
+    viewStyle() {
+        return {
+          flex: 1,
+          backgroundColor: randomcolor(),
+          justifyContent: 'center',
+          alignItems: 'center',
+        }
+    }
+
+    render(){
+        return (
+            <Swiper
+            loop={false}
+            showsPagination={false}
+            index={1}>
+            <View style={this.viewStyle()}>
+                <SwiperLeft/>
+            </View>
+            <Swiper
+                horizontal={false}
+                loop={false}
+                showsPagination={false}
+                index={1}>
+                <View style={this.viewStyle()}>
+                <Text>HOME</Text>
+                </View>
+            </Swiper>        
+            <View style={this.viewStyle()}>
+                <SwiperRight/>
+            </View>
+        </Swiper>
+        )
+    }
+}
+
+export default MainScreen;
