e85d8eca2a863f4416cffe2df6c7b9985f531a1a
JATIN <s.jatin1903@gmail.com>
2019-02-20 21:49:53 -0800

    minor stylings to right page

--- src/SwiperRight.js
+++ src/SwiperRight.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
-import {Stylesheet, View, Text} from 'react-native';
-import Swiper from 'react-native-swiper';
+import {StyleSheet, View, Text} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '85%',
+        width: '80%',
+        marginLeft: '5%',
+        marginRight: '5%',
+        borderWidth: 2,
+        borderColor: 'black',
+        backgroundColor: 'white',
+        borderRadius: 15,
+    }
+});
 
 class SwiperRight extends Component {
     render(){
         return (
-            <View>
-                <Text>Testing testing</Text>
+            <View style={styles.container}>
+                <Text>Will show the list of all scanned cards</Text>
             </View>
         );
     }
